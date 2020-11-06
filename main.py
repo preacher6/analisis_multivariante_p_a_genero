7087d233d4333164c9dc19c8b2eb2c08cd497a6d
preacher6 <juafelgrajalesg@gmail.com>
2020-11-06 12:04:55 -0500

    With samples

--- main.py
+++ main.py
@@ -22,3 +22,66 @@ df.Gender.nunique()
 # unique values of column Gender
 df.Gender.unique()
 # %%
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# %%
+# histogram of the height
+df.Height.plot(kind='hist', color='purple', edgecolor='black', figsize=(10,7))
+plt.title('Distribution of Height', size=24)
+plt.xlabel('Height (inches)', size=18)
+plt.ylabel('Frequency', size=18)
+# %%
+# histogram of the weight
+df.Weight.plot(kind='hist', color='purple', edgecolor='black', figsize=(10,7))
+plt.title('Distribution of Weight', size=24)
+plt.xlabel('Weight (pounds)', size=18)
+plt.ylabel('Frequency', size=18)
+# %%
+# histogram of the height - males and females
+df[df['Gender'] == 'Male'].Height.plot(kind='hist', color='blue', edgecolor='black', alpha=0.5, figsize=(10, 7))
+df[df['Gender'] == 'Female'].Height.plot(kind='hist', color='magenta', edgecolor='black', alpha=0.5, figsize=(10, 7))
+plt.legend(labels=['Males', 'Females'])
+plt.title('Distribution of Height', size=24)
+plt.xlabel('Height (inches)', size=18)
+plt.ylabel('Frequency', size=18)
+# %%
+# histogram of the weight - males and females
+df[df['Gender'] == 'Male'].Weight.plot(kind='hist', color='blue', edgecolor='black', alpha=0.5, figsize=(10, 7))
+df[df['Gender'] == 'Female'].Weight.plot(kind='hist', color='magenta', edgecolor='black', alpha=0.5, figsize=(10, 7))
+plt.legend(labels=['Males', 'Females'])
+plt.title('Distribution of Weight', size=24)
+plt.xlabel('Weight (pounds)', size=18)
+plt.ylabel('Frequency', size=18)
+# %%
+# Descriptive statistics male
+statistics_male = df[df['Gender'] == 'Male'].describe()
+#The rename function also accepts functions that will be applied to each column name.
+statistics_male.rename(columns=lambda x: x + '_male', inplace=True)
+# %%
+# Descriptive statistics female
+statistics_female = df[df['Gender'] == 'Female'].describe()
+statistics_female.rename(columns=lambda x: x + '_female', inplace=True)
+# %%
+# Dataframe that contains statistics for both male and female
+statistics = pd.concat([statistics_male, statistics_female], axis=1)
+statistics
+# %%
+# Scatter plot of Height and Weight
+
+ax1 = df[df['Gender'] == 'Male'].plot(kind='scatter', x='Height', y='Weight', color='blue', alpha=0.5, figsize=(10, 7))
+df[df['Gender'] == 'Female'].plot(kind='scatter', x='Height', y='Weight', color='magenta', alpha=0.5, figsize=(10 ,7), ax=ax1)
+plt.legend(labels=['Males', 'Females'])
+plt.title('Relationship between Height and Weight', size=24)
+plt.xlabel('Height (inches)', size=18)
+plt.ylabel('Weight (pounds)', size=18)
+# %%
+# Scatter plot of 500 females
+
+sample_females = df[df['Gender'] == 'Female'].sample(500)
+sample_females.plot(kind='scatter', x='Height', y='Weight', color='magenta', alpha=0.5, figsize=(10, 7))
+plt.legend(labels=['Females'])
+plt.title('Relationship between Height and Weight (sample of 500 females)', size=20)
+plt.xlabel('Height (inches)', size=18)
+plt.ylabel('Weight (pounds)', size=18)
+# %%
