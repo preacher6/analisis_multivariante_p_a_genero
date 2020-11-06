06b1946a94714658f314e55d24cb71a77e28c73a
preacher6 <juafelgrajalesg@gmail.com>
2020-11-06 15:10:58 -0500

    End

--- main.py
+++ main.py
@@ -85,3 +85,188 @@ plt.title('Relationship between Height and Weight (sample of 500 females)', size
 plt.xlabel('Height (inches)', size=18)
 plt.ylabel('Weight (pounds)', size=18)
 # %%
+import numpy as np
+
+# best fit polynomials
+
+df_males = df[df['Gender'] == 'Male']
+df_females = df[df['Gender'] == 'Female']
+
+# polynomial - males
+male_fit = np.polyfit(df_males.Height, df_males.Weight, 1)
+# array([   5.96177381, -224.49884071])
+
+# polynomial - females
+female_fit = np.polyfit(df_females.Height, df_females.Weight, 1)
+# array([   5.99404661, -246.01326575])
+# %%
+# scatter plots and regression lines
+
+# males and females dataframes
+df_males = df[df['Gender'] == 'Male']
+df_females = df[df['Gender'] == 'Female']
+# %%
+# Scatter plots.
+ax1 = df_males.plot(kind='scatter', x='Height', y='Weight', color='blue', alpha=0.5, figsize=(10, 7))
+df_females.plot(kind='scatter', x='Height', y='Weight', color='magenta', alpha=0.5, figsize=(10, 7), ax=ax1)
+# regression lines
+plt.plot(df_males.Height, male_fit[0] * df_males.Height + male_fit[1], color='darkblue', linewidth=2)
+plt.plot(df_females.Height, female_fit[0] * df_females.Height + female_fit[1], color='deeppink', linewidth=2)
+
+# regression equations
+plt.text(65, 230, 'y={:.2f}+{:.2f}*x'.format(male_fit[1], male_fit[0]), color='darkblue', size=12)
+plt.text(70, 130, 'y={:.2f}+{:.2f}*x'.format(female_fit[1], female_fit[0]), color='deeppink', size=12)
+# %%
+# with seaborn
+import seaborn as sns
+
+# regression plot using seaborn
+fig = plt.figure(figsize=(10, 7))
+sns.regplot(x=df_males.Height, y=df_males.Weight, color='blue', marker='+')
+sns.regplot(x=df_females.Height, y=df_females.Weight, color='magenta', marker='+')
+
+# Legend, title and labels.
+plt.legend(labels=['Males', 'Females'])
+plt.title('Relationship between Height and Weight', size=24)
+plt.xlabel('Height (inches)', size=18)
+plt.ylabel('Weight (pounds)', size=18)
+# %%
+
+# 300 random samples
+df_males_sample = df[df['Gender'] == 'Male'].sample(300)
+df_females_sample = df[df['Gender'] == 'Female'].sample(300)
+
+# regression plot using seaborn
+fig = plt.figure(figsize=(10, 7))
+sns.regplot(x=df_males_sample.Height, y=df_males_sample.Weight, color='blue', marker='+')
+sns.regplot(x=df_females_sample.Height, y=df_females_sample.Weight, color='magenta', marker='+')
+
+# legend, title, and labels.
+plt.legend(labels=['Males', 'Females'])
+plt.title('Relationship between Height and Weight', size=24)
+plt.xlabel('Height (inches)', size=18)
+plt.ylabel('Weight (pounds)', size=18)
+# %%
+# Fitting a simple linear model using sklearn
+from sklearn.linear_model import LinearRegression
+
+df_males = df[df['Gender'] == 'Male']
+
+# create linear regression object
+lr_males = LinearRegression()
+
+# fit linear regression
+lr_males.fit(df_males[['Height']], df_males['Weight'])
+
+# get the slope and intercept of the line best fit
+print(lr_males.intercept_)
+
+print(lr_males.coef_)
+
+df_females = df[df['Gender'] == 'Female']
+
+# create linear regression object
+lr_females = LinearRegression()
+
+# fit linear regression
+lr_females.fit(df_females[['Height']], df_females['Weight'])
+
+# get the slope and intercept of the line best fit
+print(lr_females.intercept_)
+
+print(lr_females.coef_)
+# %%
+df_females = df[df['Gender'] == 'Female']
+
+# fit the model using numpy
+female_fit = np.polyfit(df_females.Height, df_females.Weight, 1)
+
+# predictions using numpy
+print(np.polyval(female_fit, [60]))
+
+# fit the model using scikit learn
+lr_females = LinearRegression()
+lr_females.fit(df_females[['Height']], df_females['Weight'])
+
+# predictions using scikit learn
+print(lr_females.predict([[60]]))
+# %%
+# dataframe containing only females
+df_females = df[df['Gender'] == 'Female']
+
+# correlation coefficients 
+df_females.corr()
+# %%
+# dataframe containing only males
+df_males = df[df['Gender'] == 'Male']
+
+# correlation coefficients 
+df_males.corr()
+# %%
+from scipy import stats
+
+# dataframe containing only females
+df_females = df[df['Gender'] == 'Female']
+
+# pearson correlation coefficient and p-value
+pearson_coef, p_value = stats.pearsonr(df_females.Height, df_females.Weight)
+
+print(pearson_coef)
+
+# dataframe containing only males
+df_males = df[df['Gender'] == 'Male']
+
+# pearson correlation coefficient and p-value
+pearson_coef, p_value = stats.pearsonr(df_males.Height, df_males.Weight)
+
+print(pearson_coef)
+
+# %%
+# dataframe containing only females
+df_females = df[df['Gender'] == 'Female'].sample(500)
+
+# residual plot 500 females
+fig = plt.figure(figsize = (10, 7))
+sns.residplot(df_females.Height, df_females.Weight, color='magenta')
+
+# title and labels
+plt.title('Residual plot 500 females', size=24)
+plt.xlabel('Height (inches)', size=18)
+plt.ylabel('Weight (pounds)', size=18)
+# %%
+# dataframe containing only males
+df_males = df[df['Gender'] == 'Male'].sample(500)
+
+# residual plot 500 males
+fig = plt.figure(figsize=(10, 7))
+sns.residplot(df_males.Height, df_males.Weight, color='blue')
+
+# title and labels
+plt.title('Residual plot 500 males', size=24)
+plt.xlabel('Height (inches)', size=18)
+plt.ylabel('Weight (pounds)', size=18)
+
+#%%
+df_dummy = pd.get_dummies(df)
+# %%
+# drop female column
+df_dummy.drop('Gender_Female', axis=1, inplace=True)
+
+# rename Gender_Male column
+df_dummy.rename(columns={'Gender_Male': 'Gender'}, inplace=True)
+
+# df_dummy dataframe first 5 columns
+df_dummy.head()
+# %%
+from sklearn.linear_model import LinearRegression
+
+# create linear regression object
+mlr = LinearRegression()
+
+# fit linear regression
+mlr.fit(df_dummy[['Height', 'Gender']], df_dummy['Weight'])
+
+# get the slope and intercept of the line best fit.
+print(mlr.intercept_)
+print(mlr.coef_)
+# %%
